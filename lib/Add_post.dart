2de9e8e0a703eaf0c218ec522c975a22a2864174
kassem <kassemxx1>
2019-08-21 00:08:56 +0300

    select location

--- lib/Add_post.dart
+++ lib/Add_post.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:dropdown_formfield/dropdown_formfield.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'locationPicker.dart';
+
+FirebaseStorage _storage = FirebaseStorage.instance;
 final Firestore _firestore=Firestore.instance;
 class AddPost extends StatefulWidget {
   static const String id = 'AddPost_Screen';
@@ -9,10 +15,17 @@ class AddPost extends StatefulWidget {
 }
 
 class _AddPostState extends State<AddPost> {
+
+
+var _locationPickerData;
+
   var radiovalue=0;
   var delivery=false;
   var title ='';
   var PhoneNumber='';
+  List categories=[];
+  var categorieValue='';
+  var ImageLink;
   void _handeleRadio(int value){
     setState(() {
       radiovalue=value;
@@ -28,12 +41,49 @@ class _AddPostState extends State<AddPost> {
 
     });
   }
-  void getcategories(){
-   final messages = _firestore.collection('categories').getDocuments();
+  void getcategories()async{
+    categories.clear();
+   final messages =await _firestore.collection('categories').getDocuments();
+   for(var msg in messages.documents){
+     final categorie=msg.data['subcategories'];
+     setState(() {
+       categories.add({
+       'display':'$categorie',
+         'value':'$categorie',
+       });
 
+     });
 
+   }
+  }
+  Future<String> uploadPic() async {
 
-    
+    //Get the file from the image picker and store it
+    File image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    //Create a reference to the location you want to upload to in firebase
+    StorageReference reference = _storage.ref().child("images/");
+
+    //Upload the file to firebase
+    StorageUploadTask uploadTask = reference.putFile(image);
+
+    // Waits till the file is uploaded then stores the download url
+    String location = await (await uploadTask.onComplete).ref.getDownloadURL() as String;
+    print(location);
+    setState(() {
+      ImageLink = location;
+    });
+
+    //returns the download url
+    return location;
+
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getcategories();
+print(categories);
   }
   @override
   Widget build(BuildContext context) {
@@ -80,6 +130,59 @@ class _AddPostState extends State<AddPost> {
               ),
             ],
           ),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: DropDownFormField(
+              titleText: 'Categorie',
+              hintText: 'Please choose one',
+              value: categorieValue,
+              onSaved: (value) {
+                setState(() {
+                  categorieValue = value;
+                });
+              },
+              onChanged: (value) {
+
+                setState(() {
+                  categorieValue = value;
+                });
+              },
+              dataSource: categories,
+              textField: 'display',
+              valueField: 'value',
+            ),
+          ),
+          MaterialButton(
+            onPressed: (){
+              setState(() {
+                uploadPic();
+              },);
+            },
+            child: Text('upload'),
+          ),MaterialButton(
+            onPressed: () async{
+              LocationPicker.initApiKey('AIzaSyDdJJAzEqoH0RHIX06qXgMpGy188NtxmAo');
+              LocationPicker picker = new LocationPicker();
+              try
+              {
+               var result = await picker.showLocationPicker;
+               setState(() {
+                 _locationPickerData = result.toString();
+                 print(_locationPickerData);
+
+               },);
+              }
+              catch (e)
+              {
+                //HANDLE ERROR
+
+              }
+
+            },
+            child: Text('upload'),
+          )
+
+
         ],
       ),
     );
